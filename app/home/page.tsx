d17589cbae519f0d1cac041ef5a8bddae26ecf7e
mohomer512 <drprogrammer512@hotmail.com>
2025-07-15 18:22:57 +0300

    add text with anime on home

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -2,6 +2,9 @@
 
 import React from 'react'
 import {motion} from "framer-motion"
+
+import { TypeAnimation } from 'react-type-animation';
+
 import Blod from '@/components/Blod';
 import Image from 'next/image';
 import avatarImg from "@/public/assets/avatar.png";
@@ -17,7 +20,28 @@ const Home = () => {
           className="h-screen flex item-center"
           >
       <div className="flex flex-col xl:flex-row items-center justify-between w-full">
-        <div className="w-full xl:w-[550px]">text</div>
+        {/**  text **/}
+
+
+
+        
+        <div className="w-full xl:w-[550px]">
+        <h1 className="h1" >  Hi!! I`m Mohammed,   <br /> 
+        <TypeAnimation sequence={["Web Developer",2000,"IT Support Specialist",2000]} 
+        wrapper="span" speed={40} className="text-accent" repeat={Infinity} cursor={false}  />
+        
+        </h1>
+        <p>
+Begin your digital journey with us.<br/>
+Experience how your ideas effortlessly<br/>
+evolve into seamless online realities.
+        </p>
+
+        </div>
+
+         {/**  text **/}
+
+
         <div className="hidden xl:block flex-1 relative z-20">
         <Blod containerStyles="w-[560px] h-[560px]" />
           <Image src ={avatarImg} alt="" width={440} height={600}

160c778d4912e93dbdb5abaeafae272e5c2db6e2
mohomer512 <drprogrammer512@hotmail.com>
2025-07-15 22:39:50 +0300

    add contact info with icons

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -8,6 +8,8 @@ import { TypeAnimation } from 'react-type-animation';
 import Blod from '@/components/Blod';
 import Image from 'next/image';
 import avatarImg from "@/public/assets/avatar.png";
+import {MdArrowOutward} from 'react-icons/md'
+import { HiOutlineMail, HiOutlinePhone } from 'react-icons/hi';
 
 const Home = () => {
   return (
@@ -22,20 +24,49 @@ const Home = () => {
       <div className="flex flex-col xl:flex-row items-center justify-between w-full">
         {/**  text **/}
 
-
-
-        
-        <div className="w-full xl:w-[550px]">
-        <h1 className="h1" >  Hi!! I`m Mohammed,   <br /> 
+        <div className="w-full xl:w-[550px] flex-col items-center xl:items-start text-center xl:text-left">
+        <h1 className="h1 flex-1 mb-[28px]" >  Hi!! I`m Mohammed,   <br /> 
         <TypeAnimation sequence={["Web Developer",2000,"IT Support Specialist",2000]} 
         wrapper="span" speed={40} className="text-accent" repeat={Infinity} cursor={false}  />
         
         </h1>
-        <p>
-Begin your digital journey with us.<br/>
-Experience how your ideas effortlessly<br/>
-evolve into seamless online realities.
+        <p className="max-w-[500px] mb-[44px]">
+          Begin your digital journey.<br/>
+          Experience how your ideas effortlessly<br/>
+          evolve into seamless online realities.
         </p>
+
+        <button className="btn btn-lg btn-accent mb-16 items-center xl:text-center">
+          <div className="flex item-center gap-3 ">
+            <span>
+              Let`s talk.
+            </span>
+            <MdArrowOutward />
+          </div>
+
+        </button>
+        <div className="flex flex-col xl:flex-row xl:items-center gap-4 xl:gap-8
+        ">
+          <div className="flex item-center gap-4 text-lg">
+            <span className="text-accent">
+              <HiOutlinePhone />
+            </span>
+            <span>
+              +20 1123633179
+            </span>
+          </div>
+
+
+          <div className="flex item-center gap-4 text-lg">
+            <span className="text-accent">
+              <HiOutlineMail className="text-xl" />
+            </span>
+            <span>
+              drprogrammer512@hotmail.com
+            </span>
+          </div>
+
+        </div>
 
         </div>
 
@@ -45,7 +76,7 @@ evolve into seamless online realities.
         <div className="hidden xl:block flex-1 relative z-20">
         <Blod containerStyles="w-[560px] h-[560px]" />
           <Image src ={avatarImg} alt="" width={440} height={600}
-          quality={100} className="absolute -top-16 left-[120px]"
+          quality={100} className="absolute -top-16 left-[120px] mt-28"
           />
 
           <div className="w-full h-[164px]

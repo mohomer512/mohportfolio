929cbaca15d532b691b7095101f75ade841ccae5
mohomer512 <drprogrammer512@hotmail.com>
2025-07-23 23:54:28 +0300

    deploy

--- components/Gradient.tsx
+++ components/Gradient.tsx
@@ -4,5 +4,7 @@ const Gradient = () => {
   return (
     <div className='hidden md:block w-[480ox] h-[480px] rounded-full bg-accent/70 absolute -top-[30%] -right-[4%] z-30 blur-[300px]' />
   );
+
+  
 };
   export default Gradient

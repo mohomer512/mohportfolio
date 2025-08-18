649955a408826140f50a6461b15f1f606a923cf3
mohomer512 <drprogrammer512@hotmail.com>
2025-08-18 15:10:24 +0300

    download cv done

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,5 @@
 import Logo from './Logo'
-import Socials from './Socials'
 import { CiMenuFries } from 'react-icons/ci'
-import { MdFileDownload } from 'react-icons/md'
 import {Sheet,
     SheetContent,
     SheetDescription,
@@ -10,7 +8,7 @@ import {Sheet,
     SheetTrigger,
 
 } from "@/components/ui/sheet"
-import NavLinks from './NavLinks'
+import MainNav from './MainNav'
 
 const Header = () => {
   return (
@@ -18,14 +16,11 @@ const Header = () => {
             <div className='container mx-auto'>
                 <div className='flex items-center justify-between py-6'>
     <Logo />
-    {/** nav mobile sheet component */}
     <Sheet>
         <SheetTrigger className='cursor-pointer text-[30px] text-white'>
             <CiMenuFries />
         </SheetTrigger>
-        <SheetContent className='bg-primary border-0 flex flex-col justify-between
-        item-center pt-16 pb-20
-        ' side="left">
+        <SheetContent >
             <SheetHeader> 
                 <SheetTitle>
                     <Logo />
@@ -33,28 +28,9 @@ const Header = () => {
                 <SheetDescription className='sr-only' >
                     Navigation menu
                 </SheetDescription>
-                <NavLinks containerStyles='flex flex-col gap-8 max-w-[100px]' />
-                <div>
-                    
-                              <button className="btn btn-lg btn-tertiary mb-16">
-                                <div className="flex items-center gap-3">
-                                  <span>Download CV</span>
-                                  <MdFileDownload  className='text-xl'/>
-                                </div>
-                              </button>
-
-                              <Socials  
-                                 containerStyles=" flex 2xl:flex-col gap-6  2xl:flex 2xl:absolute
-        2xl:flex 2xl:absolute 2xl:top-1/2 2xl:right-2 2xl:-translate-x-1/2 2xl:-translate-y-1/2
-        " 
-        
-        iconStyles="
-        bg-accent text-white hover:bg-accent-hover transition w-[48px] h-[48px] text-[22px]
-        flex items-center justify-center rounded-full cursor-pointer
-        "
-                              />
-                    
-                </div>
+               
+                <MainNav/>
+              
             </SheetHeader>
 
         </SheetContent>
--- components/MainNav.tsx
+++ components/MainNav.tsx
@@ -11,16 +11,18 @@ const MainNav = () => {
         {/* Logo and Navigation Links */}
         <div className="flex flex-col items-center gap-12 pt-8">
           <Logo />
+          
           <NavLinks containerStyles='flex flex-col gap-6 text-gray-300' />
         </div>
 
         {/* Download CV Button */}
-        <a href="#" className="w-full text-center py-3 px-6 bg-transparent text-white border border-blue-500 rounded-lg transition-all duration-300 hover:bg-blue-500 hover:text-white hover:shadow-lg">
+        <a href="https://my.microsoftpersonalcontent.com/personal/8e37324a842fb2f9/_layouts/15/download.aspx?UniqueId=321b6bfe-a9ed-4492-8f92-35fd541e7f30&Translate=false&tempauth=v1e.eyJzaXRlaWQiOiJkZWQ2ODU1Ni05ZjczLTQ3YWEtYmFhZS0wZmNjZTI1MzMzNGYiLCJhdWQiOiIwMDAwMDAwMy0wMDAwLTBmZjEtY2UwMC0wMDAwMDAwMDAwMDAvbXkubWljcm9zb2Z0cGVyc29uYWxjb250ZW50LmNvbUA5MTg4MDQwZC02YzY3LTRjNWItYjExMi0zNmEzMDRiNjZkYWQiLCJleHAiOiIxNzU1NTIwMzUwIn0.IOu9BHAj3ZgZY6N0O_-k0EWQwxa59tB0-_3cvTcihQABb0mVOUQ9eFGL4n3dPR2qQbz3StpsMS1YUd_Dj3IrE3cWuoc_37_MzegpSw1bT0iCPiblJiUjeijcxUhhtWWwiipqVdWjuygfG7KN5BduLnmZA9pQo1_CzMG9F2bue82HRw8eWz-IK6d4bqFQCEZTY84gWEZNbYboWuOKkCFp6rx6acwUMKuL1ShhrqRct5Jzce60iRI54liRriqhQNECt1qVJU6SZymzZEfYc_MSH0hNpEgAFyRxTxzf2kbxZi35Gd5pTMMIkT-miK5h23ty3UtxoQ9EaaWkGyj8axWyBJF_lCtzRyFkHPd5B7uKq8m-W5WogwzvB3n8CDFpEFFbbS82lGAg-Ryb5LOXnCM_Kl22y8WkUHlIV3eFkgD0Ms_dlq1CCZ624GtqptmRmhMOLvksE6d_8sm2zcPZOiBHQMaiDFquBz3hBHga6CwTyqsf7YCLA0P_0bB4FGh9dJjvhchbWv9BOJKqmDqHQa_OWw.k_tX_2haxiqRfmN7uPRQW20jXlO24-pokAC-ovxImro&ApiVersion=2.0" className="w-full text-center py-3 px-6 bg-transparent text-white border border-blue-500 rounded-lg transition-all duration-300 hover:bg-blue-500 hover:text-white hover:shadow-lg">
           <div className="flex items-center justify-center gap-3">
             <span>Download CV</span>
             <MdFileDownload className='text-xl' />
           </div>
         </a>
+            
       </div>
     </nav>
   );
